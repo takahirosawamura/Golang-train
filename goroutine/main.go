8853b1f6e660928b93ea43b3453690f04b2998ee
takahirosawamura <cawst222@gmail.com>
2018-01-08 07:31:54 +0000

    [update] wait for routine not the number of loop, use array

--- goroutine/main.go
+++ goroutine/main.go
@@ -11,31 +11,39 @@ func main() {
  // チャネル
   finished := make(chan bool)
 
-  go func() {
-    // １秒かかるコマンド
-    log.Print("sleep1 started.")
-    time.Sleep(1* time.Second)
-    log.Print("sleep1 finished.")
-    finished <- true
-  }() 
+  //配列
+  funcs :=[]func(){
+    func() {
+      // １秒かかるコマンド
+      log.Print("sleep1 started.")
+      time.Sleep(1* time.Second)
+      log.Print("sleep1 finished.")
+      finished <- true
+    }, 
 
-  go func() { //2秒かかるコマンド
-    log.Print("sleep2 started.")
-    time.Sleep(2 * time.Second)
-    log.Print("sleep2 finished.")
-    finished <- true
-  }()
+    func() { //2秒かかるコマンド
+      log.Print("sleep2 started.")
+      time.Sleep(2 * time.Second)
+      log.Print("sleep2 finished.")
+      finished <- true
+    },
 
-  go func() {
-    // 3秒かかるコマンド
-    log.Print("sleep3 started.")
-    time.Sleep(3 * time.Second)
-    log.Print("sleep3 finished.")
-    finished <- true
-  }()
+    func() {
+      // 3秒かかるコマンド
+      log.Print("sleep3 started.")
+      time.Sleep(3 * time.Second)
+      log.Print("sleep3 finished.")
+      finished <- true
+    },
+  }
+
+  //並行化する
+　for _, sleep := range funcs {
+    go sleep()
+  }
 
   //終わるまで待つ(for文で回数指定させる)
-  for  i := 1; i <= 3; i++ { 
+  for  i := 0; i < len(funcs); i++  
     <- finished
   }
     log.Print("all finished.")
