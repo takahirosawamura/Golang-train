b640fe1b2d9114238d3ef69d2f9ec55173480806
takahirosawamura <cawst222@gmail.com>
2018-01-08 07:41:44 +0000

    [fix] main.go

--- goroutine/main.go
+++ goroutine/main.go
@@ -20,14 +20,12 @@ func main() {
       log.Print("sleep1 finished.")
       finished <- true
     }, 
-
     func() { //2秒かかるコマンド
       log.Print("sleep2 started.")
       time.Sleep(2 * time.Second)
       log.Print("sleep2 finished.")
       finished <- true
     },
-
     func() {
       // 3秒かかるコマンド
       log.Print("sleep3 started.")
@@ -38,13 +36,14 @@ func main() {
   }
 
   //並行化する
-　for _, sleep := range funcs {
+  for _, sleep := range funcs {
     go sleep()
   }
 
-  //終わるまで待つ(for文で回数指定させる)
-  for  i := 0; i < len(funcs); i++  
-    <- finished
+  //終わるまでfor文で回数指定させる
+  for i := 0; i < len(funcs); i++ {
+    <-finished
   }
-    log.Print("all finished.")
+
+  log.Print("all finished.")
 }
