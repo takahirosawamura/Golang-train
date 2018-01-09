1d42c19562f6ea4306c5551320c0ba184757228d
takahirosawamura <cawst222@gmail.com>
2018-01-09 12:57:45 +0000

    [add] query method

--- makeobj/main.go
+++ makeobj/main.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+  "fmt"
   "log"
 )
 
@@ -9,7 +10,15 @@ type Money struct{
   currency string
 }
 
+/*状態を変更しないクエリメソッドFormatを定義する。*/
+func (this *Money) Format() string {
+  return fmt.Sprintf("%d %s", this.amount, this.currency)
+}
+/*"%d:基数10  %s:文字列"*/
+
+
 func main() {
   money := &Money{120, "yen"}
   log.Printf("%#v",money)
+  log.Print(money.Format())
 }
