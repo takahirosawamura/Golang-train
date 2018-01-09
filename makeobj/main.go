416a8e6e8e91b99a271aa4602bc38bd0d02e7dc8
takahirosawamura <cawst222@gmail.com>
2018-01-09 13:05:51 +0000

    [add] define command method

--- makeobj/main.go
+++ makeobj/main.go
@@ -16,9 +16,13 @@ func (this *Money) Format() string {
 }
 /*"%d:基数10  %s:文字列"*/
 
+func (this *Money) Add(that *Money){
+  this.amount += that.amount
+}
 
 func main() {
   money := &Money{120, "yen"}
   log.Printf("%#v",money)
+  money.Add(&Money{180,"yen"})
   log.Print(money.Format())
 }
