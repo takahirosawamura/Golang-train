2833de19ef4520174ed703d5015116e66e9db55e
takahirosawamura <cawst222@gmail.com>
2018-01-10 10:01:42 +0000

    [add] mkdir the type of tree

--- gofile/main.go
+++ gofile/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+  "fmt"
   "os"
-  "syscall"
+//  "syscall"
 )
 
+//func main() {
+//   defaultUmask := syscall.Umask(0)
+//   os.Mkdir("/tmp/go-dir2", 0777)
+//   syscall.Umask(defaultUmask)
+//} 
+
+//os.MkdirAllを使うと、親ディレクトリがなければ親ディレクトリを作ってくるので、今回はそれを使ってコードを書く。
+//参考ページ　https://qiita.com/suin/items/af8f306dc6b38a293ef://qiita.com/suin/items/af8f306dc6b38a293ef://qiita.com/suin/items/af8f306dc6b38a293ef555
 func main() {
-   defaultUmask := syscall.Umask(0)
-   os.Mkdir("/tmp/go-dir2", 0777)
-   syscall.Umask(defaultUmask)
-} 
+  for i := 1; i <= 10; i++ {
+    os.MkdirAll(fmt.Sprintf("/tmp/go-dirs/%02d", i), os.ModePerm)
+  }
+}
