9de927596239ff62aa22cb0586e342425ce40c41
takahirosawamura <cawst222@gmail.com>
2018-01-10 10:54:13 +0000

    [update] Raw string literal

--- gofile2/main.go
+++ gofile2/main.go
@@ -4,12 +4,19 @@ import (
   "io/ioutil"
   "os"
 )
-func main() {
-  content := []byte("hello world\n" +
-		"hello world\n" +
-		"hello world\n" +
-		"hello world\n" ,
+//func main() {
+//  content := []byte("hello world\n" +
+//		"hello world\n" +
+//		"hello world\n" +
+//		"hello world\n" ,
+//  )  
 
-  )  
+func main() {
+  content := []byte(
+	`hello world
+	 hello world
+	 hello world
+	 hello world
+`)
   ioutil.WriteFile("/tmp/go-file",content, os.ModePerm)
 }
