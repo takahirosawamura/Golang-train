4a81f64a2cccfb33b87c41f6a37f0bc7d6689919
takahirosawamura <cawst222@gmail.com>
2018-01-14 08:53:01 +0000

    [update] Introducing Function Literals and Closures

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -8,6 +8,7 @@
       "html/template"
       "io/ioutil"
       "net/http"
+      "regexp"
   )
   
   type Page struct {
@@ -32,27 +33,27 @@
       return &Page{Title: title, Body: body}, nil
   }
 
-  func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-    m := validPath.FindStringSubmatch(r.URL.Path)
-    if m == nil {
-        http.NotFound(w, r)
-        return "", errors.New("Invalid Page Title")
-    }
-    return m[2], nil // The title is the second subexpression.
-  } 
 
   func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-      t, _ := template.ParseFiles(tmpl + ".html")
+      err := templates.ExecuteTemplate(w, tmpl+".html", p)
       if err != nil {
-          http.Error (w, err.Error()), http.StatusInternalServerError)
+          http.Error (w, err.Error(), http.StatusInternalServerError)
       } 
   }
- 
-  func viewHandler(w http.ResponseWriter, r *http.Request) {
-      title, err := getTitle(w, r)
-      if err != nil {
-          return
+
+  func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        m := validPath.FindStringSubmatch(r.URL.Path)
+        if m == nil {
+            http.NotFound(w, r)
+            return
+        }
+        fn(w, r, m[2])
+    }
   }
+ 
+  func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+  
   p, err := loadPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -61,11 +62,7 @@
       renderTemplate(w, "view", p)
   }
 
-  func editHandler(w http.ResponseWriter, r *http.Request) {
-    title, err := getTitle(w, r)
-    if err != nil {
-        return
-    }
+  func editHandler(w http.ResponseWriter, r *http.Request, title string) {
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
@@ -73,25 +70,22 @@
     renderTemplate(w, "edit", p)
 }
 
- 
-  func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title, err := getTitle(w, r)
-    if err != nil {
-        return
-    }
+  func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
-    err = p.save()
+    err := p.save()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
     http.Redirect(w, r, "/view/"+title, http.StatusFound)
   }
+ 
 
   func main() {
-      http.HandleFunc("/view/", viewHandler)
-      http.HandleFunc("/edit/", editHandler)
-      http.HandleFunc("/save/", saveHandler)
-      http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/view/", makeHandler(viewHandler))
+    http.HandleFunc("/edit/", makeHandler(editHandler))
+    http.HandleFunc("/save/", makeHandler(saveHandler))
+
+    http.ListenAndServe(":8080", nil)
   }
