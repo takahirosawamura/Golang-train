f051bf1e9bdc94bad35444a7c3e56d16c344d0b7
takahirosawamura <cawst222@gmail.com>
2018-02-03 09:38:16 +0000

    [update] #3 variable

--- hello.go
+++ hello.go
@@ -1,7 +1,28 @@
+//変数
+//変数名：1文字目に注意
 package main
 
 import "fmt"//format
 
 func main() {
+	//var msg string
+	//msg = "hello world"
+	//var msg = "hello world"
+	msg := "hello world"//宣言と値の代入を同時に行う。
 	fmt.Println("hello world")
+
+	//var a,b int
+	//a,b = 10,15
+	a,b := 10,15 //宣言と値の紐付け
+
+	var (
+		c int
+		d string
+	)
+	c = 20
+	d = "hoge"
 }
+
+
+
+
